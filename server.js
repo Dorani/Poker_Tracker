789ceb49ba3a686aeee28b64e0f1378f8ef5c0f4
Seif <seif.dorani@gmail.com>
2018-12-26 17:48:47 -0500

    required modules, set engine for templates, create connection to sql db, connect, create function to serve contents of db,route for generic, get request to api route for same info, then post route where we insert info about player poker stats into db

---
+++ server.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var app = express();
+var mysql = require("mysql");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+var PORT = 3000;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "handHistoryDB"
+});
+
+connection.connect(function(err){
+  if (err) throw err;
+  console.log("Connection ID: " + connection.threadId);
+  viewStats();
+});
+
+function viewStats(){
+  connection.query("SELECT * from poker_hands", function(err,data){
+    if (err) throw err;
+    console.log(JSON.stringify(data, null, 2));
+  });
+}
+
+
+app.get("/welcome", function(req,res){
+  res.send("<h1> WELCOME!! <h1>");
+});
+
+app.get("/api/player", function(req,res){
+  connection.query("Select * from poker_hands", function(err,data){
+    res.render("index", { player: data });
+  });
+});
+
+app.post("/api/player", function(req, res) {
+  var query = "INSERT INTO poker_hands (player_name, player_type, player_aggression, player_winRate, player_bb_per100, games_played) VALUES (?, ?, ?, ?, ?, ?)";
+  connection.query(query, [req.body.player_name, req.body.player_type, req.body.player_aggression, req.body.player_winRate, req.body.player_bb_per100, req.body.games_played], function(err, result) {
+    if (err) {
+      throw err;
+    }
+
+    viewStats();
+    res.json(result)
+  });
+});
+
+
+
+
+
+
+
+
+app.listen(PORT, function(){
+  console.log("we are live on port: " + PORT);
+});
